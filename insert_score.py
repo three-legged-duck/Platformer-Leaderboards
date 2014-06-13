631fdf233110524cae95b56422347f8e609bf4f1
Corentin Garcia <corenting@gmail.com>
2014-06-13 16:00:07 +0200

    Insertion functions

---
+++ insert_score.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine, and_
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+ 
+Base = declarative_base()
+ 
+class Score(Base):
+    __tablename__ = 'scores'
+    uniqueId = Column(Integer, primary_key=True)
+    worldId = Column(Integer, nullable=False)
+    levelId = Column(Integer, nullable=False)
+    name = Column(String(18), nullable=False)
+    score = Column(Integer, nullable=False)
+
+def CreateDatabase():
+    engine = create_engine('sqlite:///scores.db')
+    Base.metadata.create_all(engine)
+
+def Insert(world,level,player,points):
+    engine = create_engine('sqlite:///scores.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    score = Score(worldId = world, levelId = level, name = player, score=points)
+    session.add(score)
+    session.commit()
+
+# Insert the highscore in the database if necessary.
+# Return 0 if the score replaces an older highscore, 1 if there were less than 10 highscores, and -1 if it wasn't added
+def InsertHighscore(world,level,player,points):    
+    engine = create_engine('sqlite:///scores.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker()
+    DBSession.bind = engine
+    session = DBSession()
+    scores = session.query(Score).filter(and_(Score.worldId == world,Score.levelId == level)).all()
+    if len(scores) < 10:
+        Insert(world,level,player,points)
+        return 1
+    else:
+        minScore = scores[0].score
+        minId = scores[0].uniqueId
+        for item in scores:
+            if item.score < minScore:
+                minScore = item.score
+                minId = item.uniqueId
+        if points > minScore:
+            session.delete(session.query(Score).get(minId))
+            session.commit()
+            Insert(world,level,player,points)
+            return 0
+        return -1
